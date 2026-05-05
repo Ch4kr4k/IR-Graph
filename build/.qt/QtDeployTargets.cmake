a85f425dab340ae97fb8c12e22c421c1a71b23a4
Ch4kr4k <marakmuna9@gmail.comgtok>
2026-05-05 23:32:33 +0530

    first commit

--- build/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_IR_Graph_FILE /home/chakrak/ws/src/cpp/IR_Graph/build/IR_Graph)
-set(__QT_DEPLOY_TARGET_IR_Graph_TYPE EXECUTABLE)
--- build/CMakeFiles/IR_Graph.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for IR_Graph.
-# This may be replaced when dependencies are built.
